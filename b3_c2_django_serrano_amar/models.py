cae470ea03c29ef84806d261664003c26a5189b2
Anthony Serrano <anthony.serrano@epsi.fr>
2023-04-03 21:45:35 +0200

    Edit Ecole & Reservation models

--- b3_c2_django_serrano_amar/models.py
+++ b3_c2_django_serrano_amar/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Ecole(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=200)
     adresse = models.CharField(max_length=200)
 
 class Reservation(models.Model):
+    id = models.AutoField(primary_key=True)
     ecole = models.ForeignKey(Ecole, on_delete=models.CASCADE)
     client = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_debut = models.DateTimeField()
-    date_fin = models.DateTimeField()
+    date = models.DateTimeField()
